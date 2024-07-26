0bd95afe71bca59af407cfc03c2eef691abc316f
frinnyfrin <167410025+frinnyfrin@users.noreply.github.com>
2024-07-26 16:49:04 +0800

    Add files via upload

---
+++ Web/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__) 
+
+@app.route('/')
+def  base():    
+     return render_template("base.html")
+
+@app.route('/cart')
+def cart():
+      return render_template("cart.html")
+
+if __name__=="__main__":
+       app.run(debug=True,host='0.0.0.0')

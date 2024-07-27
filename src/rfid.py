0b5c2892c4db0796365590c97849c8e20dc2d4cb
Xavier <xaviersiao.23@ichat.sp.edu.sg>
2024-07-28 02:17:29 +0800

    Currently Working on it also >:<

--- src/rfid.py
+++ src/rfid.py
@@ -10,21 +10,22 @@ def read_rfid(reader):
     var1 = reader.read()
     sys.stdout.close()
     sys.stdout = ignore_stdout
-
+    #rfid output [uid, card info, Pin]
     print(var1)
     #clean list
     var1 = [str(item) for item in var1  ] #convert item to string
-    cleaned_var1 = [item,strip(' ') for item in var1]
+    cleaned_var1 = [item.strip(' ') for item in var1]
     var2 = cleaned_var1[1]
     split_var2 = var2.split(',')
     uid = cleaned_var1[0]
+
     try:
         card_info = [int(item) for item in split_var2[:-1]]
     except ValueError:
         card_info = [float(item) for item in split_var2[:-1]]
     pin = split_var2[-1]
 
-    return [uid, card_info, pin]
+    return [uid]
 
 if __name__ == "__main__":
     reader = rfid_reader.init()

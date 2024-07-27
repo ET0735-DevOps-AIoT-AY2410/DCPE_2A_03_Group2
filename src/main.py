36c05b385b3076c8117c2130704dffbf0e2b9927
Xavier <xaviersiao.23@ichat.sp.edu.sg>
2024-07-28 02:16:35 +0800

    Currently Working on it :<

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import time
 import queue
 import readkey as key
 import rfid as rfid
+from hal import hal_rfid_reader as rfid_reader
 
 lcd = LCD.lcd()
 shared_keypad_queue = queue.Queue()
@@ -12,6 +13,7 @@ shared_keypad_queue = queue.Queue()
 def main():
     
     key.key_reader()
+    reader = rfid_reader.init()
 
     while(True): 
         lcd.lcd_display_string("1. Scanner Start",1)
@@ -28,9 +30,19 @@ def main():
 
                 if key.ret_key() == 2:
                     lcd.clear()
-                    lcd.lcd_display_string("ATMPIN Selected",1)
-                    #input rfid code
+                    lcd.lcd_display_string("Scan your card",1)
+                    uid = rfid.rfid_reader(reader)
+                    #if rfid.read_rfid(reader) == #enter UID of tag : 
+                        lcd.clear()
+                        lcd.lcd_display_string("Type Pin number",1)
+                        if key.ATMPIN(uid) == True:
+                            
+
+                    
+                    
                     #input atm card code
+                    if key.ATMPIN(card_pin) == True:
+
 
                     
                     
